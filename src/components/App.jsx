fa588040f3202aba200e2e3a291af9ab68ff9e0e
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-05-27 22:33:34 +0300

    часть 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import React from 'react';
+import UserProfile from '../data/user.json';
+import Profile from '../components/Profile/Profile.js';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={UserProfile.username}
+        tag={UserProfile.tag}
+        location={UserProfile.location}
+        avatar={UserProfile.avatar}
+        stats={UserProfile.stats}
+      />
     </div>
   );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,65 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import PropTypes from 'prop-types';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-// import React from 'react';
-
-
-const Profile = ({ username, tag, location, avatar, stats }) => {
-  const { followers, views, likes } = stats;
-  return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
-      </div>
-
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
-  );
-};
-
-Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-  }).isRequired,
-};
-
-export default Profile;
-
-    // Импортируем React и PropTypes.
-// Создаем функциональный компонент Profile, который принимает несколько пропсов (username, tag,
-// location, avatar, stats).
-    // Разбираем объект stats, чтобы получить отдельно значения followers, views и likes.
-    // Рендерим HTML-структуру, используя значения пропсов и объекта stats.
-    // Описываем propTypes для всех пропсов, чтобы убедиться, что они передаются правильно.
-
